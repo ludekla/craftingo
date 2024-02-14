b27655aadc91f1c50d31c28ab0bd77525bdfe763
Lutz <ludewitz@gmx.de>
2024-02-15 00:56:01 +0100

    Add Visitor interface and implementation of visitor pattern (§5.3 of the book).

--- pkg/grammar/expr.go
+++ pkg/grammar/expr.go
@@ -2,25 +2,50 @@ package grammar
 
 import tk "glox/pkg/tokens"
 
-type Expr interface {
-	any
+type ReturnType interface{ string | float64 | int }
+
+type Expr[R ReturnType] interface {
+	Accept(v Visitor[R]) R
 }
 
-type Binary struct {
-	left     Expr
+type Binary[R ReturnType] struct {
+	left     Expr[R]
 	operator tk.Token
-	right    Expr
+	right    Expr[R]
 }
 
-type Grouping struct {
-	expression Expr
+type Grouping[R ReturnType] struct {
+	expression Expr[R]
 }
 
-type Literal struct {
-	value any
+type Literal[R ReturnType] struct {
+	value Expr[R]
 }
 
-type Unary struct {
+type Unary[R ReturnType] struct {
 	operator   tk.Token
-	expression Expr
+	expression Expr[R]
+}
+
+type Visitor[R ReturnType] interface {
+	VisitBinary(b Binary[R]) R
+	VisitGrouping(g Grouping[R]) R
+	VisitLiteral(l Literal[R]) R
+	VisitUnary(u Unary[R]) R
+}
+
+func (b Binary[R]) Accept(v Visitor[R]) R {
+	return v.VisitBinary(b)
+}
+
+func (g Grouping[R]) Accept(v Visitor[R]) R {
+	return v.VisitGrouping(g)
+}
+
+func (l Literal[R]) Accept(v Visitor[R]) R {
+	return v.VisitLiteral(l)
+}
+
+func (u Unary[R]) Accept(v Visitor[R]) R {
+	return v.VisitUnary(u)
 }
--- tool/expr.py
+++ tool/expr.py
@@ -1,19 +1,26 @@
 import os
+from typing import List
 
 structs = [
-    'Binary: left Expr, operator tk.Token, right Expr',
-    'Grouping: expression Expr',
-    'Literal: value any',
-    'Unary: operator tk.Token, expression Expr'
+    'Binary[R ReturnType]: left Expr[R], operator tk.Token, right Expr[R]',
+    'Grouping[R ReturnType]: expression Expr[R]',
+    'Literal[R ReturnType]: value Expr[R]',
+    'Unary[R ReturnType]: operator tk.Token, expression Expr[R]'
 ]
 
 top_section = '''package grammar
 
 import tk "glox/pkg/tokens"
 
-type Expr interface {
-    any
-}'''
+type ReturnType interface { string | float64 | int }'''
+
+def define_expr(exprs: List[str]):
+    parts = [
+        'type Expr[R ReturnType] interface {',
+        f'    Accept(v Visitor[R]) R',
+        '}'
+    ]
+    return '\n'.join(parts)
 
 def setup_struct(expr: str):
     clsname, signature = expr.split(': ')
@@ -23,13 +30,44 @@ def setup_struct(expr: str):
     bottom = '}'
     return '\n'.join([top, body, bottom])
 
+def setup_visitor(exprs: List[str]):
+    parts = ['type Visitor[R ReturnType] interface {']
+    for expr in exprs:
+        clsname, *_ = expr.split(':')
+        clsname = clsname.replace('ReturnType', '').strip()
+        n = clsname.find('[')
+        pure_clsname = clsname[:n]
+        parts.append(f'   Visit{pure_clsname}({clsname[0].lower()} {clsname}) R')
+    parts.append('}')
+    return '\n'.join(parts)
+
+def implement_visitor(exprs: List[str]):
+    parts = []
+    for expr in exprs:
+        clsname, *_ = expr.split(':')
+        obj = clsname[0].lower()
+        clsname = clsname.replace('ReturnType', '').strip()
+        n = clsname.find('[')
+        pure_clsname = clsname[:n]
+        code_lines = [
+            f'func ({obj} {clsname}) ' + 'Accept(v Visitor[R]) R {',
+            f'    return v.Visit{pure_clsname}({obj})',
+            '}'
+        ]
+        parts.append('\n'.join(code_lines))
+    return '\n\n'.join(parts)
+
 def write():
     with open('pkg/grammar/expr.go', mode='w') as fout:
         fout.write(top_section + '\n\n')
+        fout.write(define_expr(structs) + '\n\n')
         for struct in structs:
             struct_str = setup_struct(struct)
             fout.write(struct_str + '\n\n')
-
+        visitor = setup_visitor(structs)
+        fout.write(visitor + '\n\n')
+        impls = implement_visitor(structs)
+        fout.write(impls)
 
 
 if __name__ == '__main__':
